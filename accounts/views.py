2528c6e63f74e9a59cf49baf030b12ddc04b56bc
Beale Stainton <bealeness@gmail.com>
2021-05-06 18:20:51 +1200

    home page created with dummy data, still needs database

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+jobs = [
+    {
+        'company': 'Google',
+        'role': 'Python Developer',
+        'contact': 'google@gmail.com',
+        'deadline': 'June 2 2021'
+    },
+    {
+        'company': 'Microsoft',
+        'role': 'Python Developer',
+        'contact': 'microsoft@hotmail.com',
+        'deadline': 'June 12 2021'
+    }
+]
+
 @login_required
 def profile(request):
-    return render(request, 'accounts/profile.html')
+    context = {
+        'jobs': jobs
+    }
+    return render(request, 'accounts/profile.html', context)
--- job_lead/settings.py
+++ job_lead/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = os.path.join(BASE_DIR, 'static') 
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'job_lead.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join((BASE_DIR), 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
